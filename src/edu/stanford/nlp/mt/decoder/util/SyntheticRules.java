aa7b3b6211d9f7adf78fcf723744f214960ba0e9
Chase Tingley <chase@lilt.com>
2025-07-24 14:54:20 -0700

    Update gradle version; bump commons-lang3; fix a typing issue
        3.8.0-SNAPSHOT

--- src/edu/stanford/nlp/mt/decoder/util/SyntheticRules.java
+++ src/edu/stanford/nlp/mt/decoder/util/SyntheticRules.java
@@ -378,7 +378,7 @@ public final class SyntheticRules {
           cov.set(r.fi, r.fj);
           Set<Integer>[] e2f = new TreeSet[tgt.size()];
           for (int eIdx = r.ei; eIdx < r.ej; ++eIdx) {
-            e2f[eIdx - r.ei] = sym.e2f(eIdx).stream().mapToInt(a -> a - r.fi).boxed().collect(Collectors.toCollection(() -> new TreeSet()));
+            e2f[eIdx - r.ei] = sym.e2f(eIdx).stream().mapToInt(a -> a - r.fi).boxed().collect(Collectors.toCollection(() -> new TreeSet<>()));
           }
 
           PhraseAlignment alignment = new PhraseAlignment(e2f);
